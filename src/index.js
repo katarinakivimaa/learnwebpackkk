5a5135c6d613d34ab2735d8f045bf982234c2406
Katarina Kivimaa <katarina.kivimaa@gmail.com>
2023-12-06 15:22:05 +0200

    first commit

---
+++ src/index.js
@@ -0,0 +1,27 @@
+import './style.scss';
+
+import * as bootstrap from 'bootstrap';
+import { createApp } from 'vue';
+import * as VueRouter from 'vue-router';
+
+import App from './App.vue';
+import ToDo from './pages/ToDo.vue';
+import Home from './pages/Home.vue';
+import Clicker from './pages/Clicker.vue';
+import RickAndMorty from './pages/RickAndMorty.vue';
+
+const routes = [
+ {path: '/', component: Home},
+ {path: '/todo', component: ToDo},
+ {path: '/clicker', component: Clicker},
+ {path: '/rickandmorty', component: RickAndMorty},
+];
+
+const router = VueRouter.createRouter({
+    history: VueRouter.createWebHistory(),
+    routes
+});
+
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import fs from 'fs';
+import glob from 'glob';
+import { PurgeCSSPlugin } from 'purgecss-webpack-plugin';
+import { VueLoaderPlugin } from 'vue-loader';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default {
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname, 'public')
+        },
+        historyApiFallback: true,
+        open: true,
+        port: 9000,
+        compress: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader,'css-loader']
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [MiniCssExtractPlugin.loader,'css-loader', 'sass-loader']
+            },
+            {
+                test: /\.nunjucks$/i,
+                use: ['simple-nunjucks-loader']
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        }),
+        new HtmlWebpackPlugin({
+            filename: '200.html',
+            template: './src/index.html'
+        }),
+        new MiniCssExtractPlugin(),
+        // new PurgeCSSPlugin({
+        //     paths: glob.sync(`src/views/**/*`, { nodir: true }),
+        // }),
+        new VueLoaderPlugin()
+    ],
+}
